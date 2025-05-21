ac877e71057272558bb8b7bea7afcf732a356ba7
Adil-Solkar <adilsolkar20@gmail.com>
2025-05-21 10:31:43 +0530

    add React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
+import Body from "./Components/Body"
 function App() {
-
-
   return (
     <>
-    <h1 className="m-3 p-2 border">Netflix</h1>
+      <Body />
     </>
   )
 }
 
-export default App
+export default App 
---
+++ src/Components/Body.jsx
@@ -0,0 +1,25 @@
+import Login from "./Login"
+import Browse from "./Browse"
+import { createBrowserRouter } from "react-router"
+import { RouterProvider } from "react-router"
+
+function Body() {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />
+    },
+    {
+      path: "/browse",
+      element: <Browse />
+    },
+  ])
+
+  return (
+    <>
+      <RouterProvider router={appRouter}/>
+    </>
+  )
+}
+
+export default Body 
