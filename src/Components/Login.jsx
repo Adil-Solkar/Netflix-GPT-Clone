fec1ff327a9e2742baebbcd451a2e3f5cb53368f
Adil-Solkar <adilsolkar20@gmail.com>
2025-05-29 16:24:36 +0530

    Add firebase config && Sign up and Sign in logic

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,10 +1,15 @@
 import { useRef, useState } from "react";
 import Header from "./Header";
-import {validateFormData} from '../utils/validate'
+import { validateFormData } from "../utils/validate";
+import { auth } from "../utils/firebaseConfig";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 const Login = () => {
   const [isSignUp, setIsSignUp] = useState(false);
-  const [errorMessage,setErrorMessage] = useState(null)
+  const [errorMessage,setErrorMessage] = useState(null);
 
   const fullName = useRef(null);
   const email = useRef(null);
@@ -12,58 +17,113 @@ const Login = () => {
 
   function handleSignUpClick() {
     setIsSignUp((prevState) => !prevState);
+    setErrorMessage(null);
   }
 
-  function handleButtonClick(e){
-    e.preventDefault()
-    const errorMessage = validateFormData(fullName?.current?.value,email?.current?.value,password?.current?.value,isSignUp)
-    // console.log(errorMessage)
-    setErrorMessage(errorMessage)
+  function handleButtonClick(e) {
+    e.preventDefault();
+    const errorMessage = validateFormData(
+      fullName?.current?.value,
+      email?.current?.value,
+      password?.current?.value,
+      isSignUp
+    );
+    setErrorMessage(errorMessage);
+
+    if (errorMessage) return;
+
+    if (!errorMessage) {
+      if (isSignUp) {
+        // Sign up logic
+        createUserWithEmailAndPassword(
+          auth,
+          email?.current?.value,
+          password?.current?.value
+        )
+          .then((userCredential) => {
+            const user = userCredential.user;
+            console.log(user);
+          })
+          .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            setErrorMessage(errorCode + " " + ": " + errorMessage);
+          });
+        //  fullName.current.value = ""
+        //  email.current.value = ""
+        //  password.current.value = ""
+      } else {
+        // Sign in logic
+        signInWithEmailAndPassword(
+          auth,
+          email?.current?.value,
+          password?.current?.value
+        )
+          .then((userCredential) => {
+            const user = userCredential.user;
+            console.log(user);
+          })
+          .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            setErrorMessage(errorCode + " " + ": " + errorMessage);
+          });
+      }
+    }
   }
 
   return (
     <>
-      <Header />
-      <div className="flex justify-center">
-        <div className="border bg-black opacity-90 m-5 min-w-lg">
-          <form className="flex flex-col p-12">
-            <h2 className="text-2xl text-white font-bold mb-8">
-              {isSignUp ? "Sign up" : "Sign In"}
-            </h2>
-            {isSignUp && (
+      <div className="bg-gray-800 min-h-screen">
+        <Header />
+        <div className="flex justify-center mt-10">
+          <div className="border bg-black opacity-90 m-5 min-w-lg">
+            <form className="flex flex-col p-12">
+              <h2 className="text-2xl text-white font-bold mb-8">
+                {isSignUp ? "Sign up" : "Sign In"}
+              </h2>
+              {isSignUp && (
+                <input
+                  ref={fullName}
+                  type="text"
+                  placeholder="Full Name"
+                  className="mb-5 p-3 border border-white text-white "
+                />
+              )}
               <input
-                ref={fullName}
+                ref={email}
                 type="text"
-                placeholder="Full Name"
+                placeholder="Email Address"
                 className="mb-5 p-3 border border-white text-white "
               />
-            )}
-            <input
-              ref={email}
-              type="text"
-              placeholder="Email Address"
-              className="mb-5 p-3 border border-white text-white "
-            />
-            <input
-              ref={password}
-              type="password"
-              placeholder="Password"
-              className="mb-5 p-3 border border-white text-white "
-            />
-            {errorMessage && <p className="text-red-500 text-lg font-bold tracking-wide mb-5">{errorMessage}</p>}
-            <button onClick={handleButtonClick} className="bg-red-600 p-3 text-white rounded-md mb-5">
-              {isSignUp ? "Sign up" : "Sign In"}
-            </button>
-            <p className="text-white">
-              {isSignUp ? "Already a user?" : "New to Netflix?"}
-              <span
-                className="cursor-pointer hover:text-amber-100"
-                onClick={handleSignUpClick}
+              <input
+                ref={password}
+                type="password"
+                placeholder="Password"
+                className="mb-5 p-3 border border-white text-white "
+              />
+              {errorMessage && (
+                <p className="text-red-500 text-lg font-bold tracking-wide mb-5">
+                  {errorMessage}
+                </p>
+              )}
+              <button
+                onClick={handleButtonClick}
+                className="bg-red-600 p-3 text-white rounded-md mb-5"
               >
-                {isSignUp ? " Sign In" : " Sign up now."}
-              </span>
-            </p>
-          </form>
+                {isSignUp ? "Sign up" : "Sign In"}
+              </button>
+              <p className="text-white">
+                {isSignUp ? "Already a user?" : "New to Netflix?"}
+                <span
+                  className="cursor-pointer hover:text-amber-100"
+                  onClick={handleSignUpClick}
+                >
+                  {isSignUp ? " Sign In" : " Sign up now."}
+                </span>
+              </p>
+            </form>
+          </div>
         </div>
       </div>
     </>
