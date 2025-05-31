f3427d73d992aa5bc745287eee3c9a85ffcb549c
Adil-Solkar <adilsolkar20@gmail.com>
2025-05-31 16:06:51 +0530

    Add sign out

--- src/Components/Browse.jsx
+++ src/Components/Browse.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import Header from "./Header";
+import { auth } from "../utils/firebaseConfig";
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router";
 
 const Browse = () => {
-  return (
-    <div>Browse</div>
-  )
-}
 
-export default Browse
+  const navigate = useNavigate()
+
+  function handleSignOutClick() {
+   signOut(auth) 
+      .then(() => {
+        // Sign-out successful.
+        navigate("/")
+      })
+      .catch((error) => {
+        // An error happened.
+        console.log(error.message)
+        navigate("/error") // build error page and use error object
+      });
+  }
+
+  return (
+    <>
+      <div className="flex">
+        <Header />
+        <button
+          onClick={handleSignOutClick}
+          className="bg-red-500 text-white mx-4 my-2 px-3 rounded-md hover:bg-red-600 transition duration-200 whitespace-nowrap text-xl"
+        >
+          Sign Out
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Browse;
+
+/**
+ *
+
+
+ * 
+ */
